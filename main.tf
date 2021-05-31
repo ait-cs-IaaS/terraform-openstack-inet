b89a1c7b6b07d11519d2f1a2dff58d0f70514f01
Maximilian Frank <maximilian.frank@ait.ac.at>
2021-05-31 12:15:04 +0200

    Update module to use openstack-srv_noportsec for DNS host

--- main.tf
+++ main.tf
@@ -1,86 +1,37 @@
-terraform {
-  backend "consul" {}
-}
-
-data "openstack_images_image_v2" "dns" {
-  name        = var.dns_image
-  most_recent = true
-}
-
 data "openstack_networking_router_v2" "public_router" {
   name = var.router_name
 }
 
-data "template_file" "user_data" {
-  template = "${file("${path.module}/scripts/dnscloudinit.yml")}"
-}
-
-data "template_cloudinit_config" "cloudinit" {
-  gzip          = false
-  base64_encode = false
-
-  part {
-    filename     = "init.cfg"
-    content_type = "text/cloud-config"
-    content      = data.template_file.user_data.rendered
-  }
-}
-
 resource "openstack_networking_network_v2" "internet" {
-  name           = "internet"
+  name           = var.internet_name
   admin_state_up = "true"
 }
 
 resource "openstack_networking_subnet_v2" "inet-subnet" {
-  name       = "subnet-internet"
-  network_id = openstack_networking_network_v2.internet.id
-  cidr       = var.cidr
+  name            = "subnet-${var.internet_name}"
+  network_id      = openstack_networking_network_v2.internet.id
+  cidr            = var.cidr
   dns_nameservers = var.external_dns
-  ip_version = 4
+  ip_version      = 4
 }
-
-
 
 resource "openstack_networking_router_interface_v2" "router_interface_1" {
   router_id = data.openstack_networking_router_v2.public_router.id
   subnet_id = openstack_networking_subnet_v2.inet-subnet.id
 }
 
-# Create dns server instance
-resource "openstack_compute_instance_v2" "dns" {
-  name               = "internet-dns"
-  flavor_name        = var.dns_flavor
-  key_pair           = var.dns_sshkey
-
-  user_data = data.template_cloudinit_config.cloudinit.rendered
-
-  metadata = {
-    groups = "internet, dns"
-  }
-
-  block_device {
-    uuid                  = data.openstack_images_image_v2.dns.id
-    source_type           = "image"
-    volume_size           = 10
-    boot_index            = 0
-    destination_type      = "volume"
-    delete_on_termination = true
-  }
-
-  network {
-    port = openstack_networking_port_v2.inetdns-port.id
-  }
+module "dns" {
+  source             = "git@git-service.ait.ac.at:sct-cyberrange/terraform-modules/openstack-srv_noportsec.git?ref=v1.4.1"
+  hostname           = var.dns_name
+  tag                = var.dns_tag
+  host_address_index = var.dns_host_address_index
+  image              = var.dns_image
+  flavor             = var.dns_flavor
+  volume_size        = var.dns_volume_size
+  use_volume         = var.dns_use_volume
+  sshkey             = var.sshkey
+  network            = openstack_networking_network_v2.internet.id
+  subnet             = openstack_networking_subnet_v2.inet-subnet.id
+  userdatafile       = var.dns_userdata == null ? "${path.module}/scripts/dnscloudinit.yml" : var.dns_userdata
+  userdata_vars      = var.dns_userdata_vars
 }
-
-resource "openstack_networking_port_v2" "inetdns-port" {
-  name           = "inetdns-port"
-  network_id     = openstack_networking_network_v2.internet.id
-  admin_state_up = "true"
-  no_security_groups = true
-  port_security_enabled = false
-  fixed_ip { 
-      subnet_id = openstack_networking_subnet_v2.inet-subnet.id
-      ip_address = var.dns_ip
-  }
-}
-
--- variables.tf
+++ variables.tf
@@ -1,39 +1,82 @@
 variable "cidr" {
-	type = string
-	description = "Subnet-ID"
-	default = "240.168.200.0/24"
-}
-
-variable "dns_ip" {
-	type = string
-	description = "Subnet-ID"
-	default = "240.168.200.3"
-}
-
-variable "external_dns" {
-        type = list(string)
-        description = "DNS server"
-        default = [ "8.8.8.8" ]
-}
-
-variable "dns_image" {
-	type = string
-	description = "image to boot the dns-server from"
-}
-
-variable "dns_flavor" {
-	type = string
-	description = "instance flavor for the dns-server"
-	default = "m1.tiny"
-}
-
-variable "dns_sshkey" {
-	type = string
-	description = "ssh key of the firewall"
-	default = "cyberrange-key"
+  type        = string
+  description = "The CIDR to use for the internet networks subnet"
+  default     = "240.168.200.0/24"
 }
 
 variable "router_name" {
-	type = string
-	description = "Name of the public router"
+  type        = string
+  description = "Name of the public router"
+}
+
+variable "internet_name" {
+  type        = string
+  description = "Name of the internet network to create"
+  default     = "internet"
+}
+
+variable "dns_name" {
+  type        = string
+  description = "The name to use for the internet DNS server"
+  default     = "internet-dns"
+}
+
+variable "dns_tag" {
+  type        = string
+  description = "The groups to tag the DNS server for"
+  default     = "internet, dns"
+}
+
+variable "dns_host_address_index" {
+  type        = number
+  description = "The host address index to use for the DNS server"
+  default     = 3
+}
+variable "dns_image" {
+  type        = string
+  description = "image to boot the dns-server from"
+}
+
+variable "dns_volume_size" {
+  type        = string
+  description = "volume_size"
+  default     = 10
+}
+
+variable "dns_use_volume" {
+  type        = bool
+  description = "If the a volume or a local file should be used for storage"
+  default     = false
+}
+
+variable "dns_flavor" {
+  type        = string
+  description = "instance flavor for the DNS server"
+  default     = "m1.tiny"
+}
+
+variable "dns_userdata" {
+  type        = string
+  description = "path to userdata file"
+  default     = null
+}
+
+variable "dns_userdata_vars" {
+  type        = map(string)
+  description = "variables for the userdata template"
+  default = {
+    external_dns = "8.8.8.8"
+  }
+}
+
+variable "sshkey" {
+  type        = string
+  description = "ssh key for the DNS server"
+  default     = "cyberrange-key"
+}
+
+variable "external_dns" {
+  type        = list(string)
+  description = "List of external DNS servers to configure for the internet network zone"
+  default     = ["8.8.8.8"]
 }
